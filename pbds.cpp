708c619323a389c3937713b05ffdeec90d99bf2a
Shivam Prakash <85667261+shivam-72@users.noreply.github.com>
2021-08-02 12:21:48 +0530

    Add files via upload

---
+++ pbds.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+           tree_order_statistics_node_update>PBDS;
+int main()
+{
+    ;
+    return 0;
+}
